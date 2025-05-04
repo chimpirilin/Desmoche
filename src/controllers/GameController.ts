a647b11ef96a9e824e950e57e3cc988e61503db6
Hans Blanco <hansblancodev@gmail.com>
2025-05-03 22:32:39 -0700

    add events

--- src/controllers/GameController.ts
+++ src/controllers/GameController.ts
@@ -1,9 +1,34 @@
 import { GameModel } from "../models/Game";
+import { GameEventBus } from "../events/GameEventBus";
 
 export class GameController {
     private gameModel: GameModel
 
     constructor(playerCount: number) {
         this.gameModel = new GameModel(playerCount)
+    }
+
+    // NOTE: we might not need a controller
+    // event handlers should be enough
+
+    private suscribeToEvents() {
+        const eventBus = new GameEventBus()
+        eventBus.onDrawCard((data) => {
+            console.log("Card drawn:", data.cardId);
+            // Handle card drawn event
+        });
+        eventBus.onDiscardCard((data) => {
+            console.log("Card discarded:", data.index);
+            // Handle card discarded event
+        });
+
+        eventBus.onGameStarted(() => {
+            console.log("Game started");
+            this.play();
+        });
+    }
+   
+    public play() {
+
     }
 }
--- src/models/Game.ts
+++ src/models/Game.ts
@@ -4,37 +4,37 @@ import { HumanPlayer } from "./HumanPlayer"
 import { DiscardPile } from "./DiscardPile"
 import { Player } from "./Player"
 
-// NOTE: this code mixes both model and controller functionality, refactor it to separate concerns
-// create a GameModel that holds the state of the game (e.g., players, deck, discard pile).
-// create a GameController class that holds game initialization, turn management, and player actions
+import { GameEventBus } from "../events/GameEventBus"
 
 export class GameModel {
     private botPlayers: BotPlayer[] = []
     // private humanPlayer: HumanPlayer
-    private humanPlayer: HumanPlayer[] = []
+    private humanPlayer: HumanPlayer
     private _players: Player[]
     private deck: Deck
     private discardPile: DiscardPile
     private currentPlayer: Player
     private currentPlayerIndex: number = 0
 
+    private eventEmitter: GameEventBus
+
     constructor(private playerCount: number) {
         this.deck = new Deck()
         this.discardPile = new DiscardPile()
         
-        // this.humanPlayer = new HumanPlayer(this.deck, this.discardPile, 'Human')
+        this.humanPlayer = new HumanPlayer(this.deck, this.discardPile, 'Human')
         // we substract one because we don't count the human player
-        // for(let i = 0; i < this.playerCount - 1; i++) {
-        //     this.botPlayers.push(new BotPlayer(this.deck, this.discardPile, `Bot ${i}`))
-        // }
-
+        // Rememeber to substract one wehn when we finish human player
         for(let i = 0; i < this.playerCount; i++) {
-            this.humanPlayer.push(new HumanPlayer(this.deck, this.discardPile, `Human ${i}`))
+            this.botPlayers.push(new BotPlayer(this.deck, this.discardPile, `Bot ${i}`))
         }
 
-        this._players = [...this.humanPlayer, ...this.botPlayers]
+        // uncomment this line to add the human player to the game
+        // this._players = [this.humanPlayer, ...this.botPlayers]
+        this._players = [...this.botPlayers]
         this.currentPlayer = this.determineInitialPlayer()
 
+        this.eventEmitter = new GameEventBus
     }
 
     private determineInitialPlayer(): Player {
@@ -49,6 +49,10 @@ export class GameModel {
 
     public get players(): Player[] {
         return this._players;
+    }
+
+    public startGame() {
+        this.eventEmitter.emitGameStarted()
     }
 }
 
