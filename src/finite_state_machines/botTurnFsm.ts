982bca751d2c5b84971e8aaeda3352ed55032b87
Hans Blanco <hansblancodev@gmail.com>
2025-08-25 02:43:35 -0600

    add logs, fix typo, fix guard/action/target weird behaviour by removing individual action

--- src/finite_state_machines/botTurnFsm.ts
+++ src/finite_state_machines/botTurnFsm.ts
@@ -14,6 +14,7 @@ export function createBotMachine(bot: BotPlayer, animationsEnabled: boolean = tr
                 await drawfromDiscardPile()
             }),
             drawFromDeckAnimation: fromPromise(async () => {
+                console.log('drawFromDeckAnimation actor started')
                 await drawFromDeck()
             }),
             discardAnimation: fromPromise(async () => {
@@ -27,16 +28,27 @@ export function createBotMachine(bot: BotPlayer, animationsEnabled: boolean = tr
             isFirstTurn: () => GameModel.isFirstPlay(),
             isCardOnTopOfDiscardPileUseful: ({context}) => context.bot.isCardOnTopOfDiscardPileUseful(),
             canMeld: ({context}) => context.bot.canMeld(),
-            shouldAnimate: ({context}) => context.shouldAnimate
+            shouldAnimate: ({context}) => {
+                console.log('shouldAnimate guard evaluated to', context.animationsEnabled)
+                return context.animationsEnabled
+            }
         },
         actions: {
             setFirstTurnToFalse: () => {
                 if(GameModel.isFirstPlay()) GameModel.setFirstPlayToFalse()
             },
-            drawFromTheDeck: ({context}) => context.bot.drawFromDeck(),
+            drawFromTheDeck: ({context}) => {
+                console.log('action drawFromTheDeck started')
+                context.bot.drawFromDeck()
+                console.log('action drawFromTheDeck ended')
+            },
             drawfromTheDiscardPile: ({context}) => context.bot.drawFromDiscardPile(),
             meld: ({context}) => context.bot.meld(),
-            discard: ({context}) => context.bot.discard()
+            discard: ({context}) => {
+                console.log('action discard started')
+                context.bot.discard()
+                console.log('action discard ended')
+            }
         }
     }).createMachine({
         id: 'botTurn',
@@ -47,27 +59,31 @@ export function createBotMachine(bot: BotPlayer, animationsEnabled: boolean = tr
         },
         states: {
             start: {
+                entry: () => console.log('start state entered'),
                 description: 'let copilot do it',
-                always: 'determiningIfDiscaredCardIsUseful'
+                always: 'determiningIfDiscardedCardIsUseful',
             },
-            determiningIfDiscaredCardIsUseful: {
+            determiningIfDiscardedCardIsUseful: {
+                entry: () => console.log('determiningIfDiscardedCardIsUseful state entered'),
                 always: [
                     // we probably need an action to be performed if it's the first play of the game
-                    // it will just set firPlay to false
+                    // it will just set firstPlay to false
                     // dont forget to add that later
                     {guard: 'isCardOnTopOfDiscardPileUseful', target: 'drawingFromDiscardPileAnimation'},
                     {target: 'drawingFromDeck'}
                 ]
             },
             drawingFromDiscardPile: {
+                entry: () => console.log('drawingFromDiscardPile state entered'),
                 always: [
                     // if this action doesn't behave as expected, add target: can meld to to other two below
                     {actions: 'drawfromTheDiscardPile'},
-                    {guard: 'shouldAnimate', target: 'drawFromDiscardPileAnimation'},
+                    {guard: 'shouldAnimate', target: 'drawingFromDiscardPileAnimation'},
                     {target: 'canMeld'}
                 ]
             },
             drawingFromDiscardPileAnimation: {
+                entry: () => console.log('drawingFromDiscardPileAnimation state entered'),
                 invoke: {
                     src: 'drawFromDiscardPileAnimation',
                     onDone: {
@@ -77,15 +93,19 @@ export function createBotMachine(bot: BotPlayer, animationsEnabled: boolean = tr
                 }
             },
             drawingFromDeck: {
+                entry: () => console.log('drawingFromDeck state entered'),
+
                 always: [
                     // if this action doesn't behave as expected, add target: can meld to to other two below
-                    {actions: 'drawFromTheDeck'},
-                    {guard: 'shouldAnimate', target: 'drawingFromDeckAnimation'},
-                    {target: 'canMeld'}
+                    // {actions: 'drawFromTheDeck'},
+                    {guard: 'shouldAnimate', actions: 'drawFromTheDeck', target: 'drawingFromDeckAnimation'},
+                    {actions: 'drawFromTheDeck', target: 'canMeld'}
                 ]
             },
             drawingFromDeckAnimation: {
+                // entry: () => console.log('drawingFromDeckAnimation state entered'),
                 invoke: {
+                    entry: () => console.log('drawingFromDeckAnimation state entered'),
                     src: 'drawFromDeckAnimation',
                     onDone: {
                         target: 'canMeld',
@@ -94,35 +114,40 @@ export function createBotMachine(bot: BotPlayer, animationsEnabled: boolean = tr
                 }
             },
             canMeld: {
+                entry: () => console.log('canMeld state entered'),
                 always: [
                     {guard: 'canMeld', target: 'melding'},
                     {target: 'discarding'},
                 ]
             },
             melding: {
+                entry: () => console.log('melding state entered'),
                 always: [
-                    {actions: 'meld'},
-                    {guard: 'shouldAnimate', target: 'meldingAnimation'},
-                    {target: 'discard'}
+                    // {actions: 'meld'},
+                    {guard: 'shouldAnimate', actions: 'meld', target: 'meldingAnimation'},
+                    {actions: 'meld', target: 'discarding'}
                 ]
             },
             meldingAnimation: {
+                entry: () => console.log('meldingAnimation state entered'),
                 invoke: {
                     src: 'meldAnimation',
                     onDone: {
-                        target: 'discard',
+                        target: 'discarding',
                         actions: () => console.log('meldingAnimation ended qq')
                     },
                 }
             },
             discarding: {
+                entry: () => console.log('discarding state entered'),
                 always: [
-                    {actions: 'discard'},
-                    {guard: 'shouldAnimate', target: 'discardingAnimation'},
-                    {target: 'isFirstTurn'}
+                    // {actions: 'discard'},
+                    {guard: 'shouldAnimate', actions: 'discard', target: 'discardingAnimation'},
+                    {actions: 'discard', target: 'isFirstTurn'}
                 ]
             },
             discardingAnimation: {
+                entry: () => console.log('discardingAnimation state entered'),
                 invoke: {
                     src: 'discardAnimation',
                     onDone: {
@@ -132,12 +157,14 @@ export function createBotMachine(bot: BotPlayer, animationsEnabled: boolean = tr
                 }
             },
             isFirstTurn: {
+                entry: () => console.log('isFirstTurn state entered'),
                 always: [
                     {guard: 'isFirstTurn', actions: 'setFirstTurnToFalse', target: 'drawingFromDeck'},
                     {actions: 'setFirstTurnToFalse', target: 'turnEnd'}
                 ]
             },
             turnEnd: {
+                entry: () => console.log('turnEnd state entered'),
                 type: 'final'
             }
         }
