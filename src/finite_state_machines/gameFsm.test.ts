830e41880fef8dceb795d99ad8428f5f1f9bcf37
Hans Blanco <hansblancodev@gmail.com>
2025-08-25 02:44:39 -0600

    game test blueprint

--- src/finite_state_machines/gameFsm.test.ts
+++ src/finite_state_machines/gameFsm.test.ts
@@ -1,1 +1,14 @@
-// IMPLEMENT ME
+import { describe, expect, test } from 'vitest';
+import { Deck } from '../models/Deck';
+import { DiscardPile } from '../models/DiscardPile';
+import { BotPlayer } from '../models/BotPlayer';
+import { Actor } from 'xstate';
+
+describe('Bot turn FSM', () => {
+    // IMPLEMENT ME
+    test.skip('foo', () => {
+        const deck: Deck = new Deck();
+        const discardPile: DiscardPile = new DiscardPile();
+        const botPlayer: BotPlayer = new BotPlayer(deck, discardPile, 'testBot');
+    })
+})
